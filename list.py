716e0e4f20faf0d69f5aac71ab10c78319dea841
annasebas123 <annasebas2207@gmail.com>
2019-02-07 09:59:15 +0530

    list name

---
+++ list.py
@@ -0,0 +1,8 @@
+vegetables=["carrot","cabbage","potato","tomato","onion"]
+print(vegetables)
+print(vegetables[0])
+print(vegetables[0:2])
+vegetables[0]="chilly"
+print(vegetables)
+del(vegetables)[1]
+print(vegetables)
